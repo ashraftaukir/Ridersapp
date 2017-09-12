e7a0826870a3742e8f7758fefb80b7c37549f67e
ashraftaukir <ashraftaukir@gmail.com>
2017-09-12 13:53:05 +0600

    login page design

--- app/src/main/java/com/webxzen/ridersapp/view/fragment/LoginFragment.java
+++ app/src/main/java/com/webxzen/ridersapp/view/fragment/LoginFragment.java
@@ -3,21 +3,59 @@ package com.webxzen.ridersapp.view.fragment;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.webxzen.ridersapp.R;
+import com.webxzen.ridersapp.view.MainActivity;
 
 
-public class LoginFragment extends Fragment {
+public class LoginFragment extends Fragment implements View.OnClickListener {
 
-View view;
+
+    Button loginwithfbbtn, loginwitheamilbtn;
+    TextView registrationtv;
+
+    View view;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        //return super.onCreateView(inflater, container, savedInstanceState);
-    view=inflater.inflate(R.layout.loginscreen,container,false);
-            return view;
+        view = inflater.inflate(R.layout.loginscreen, container, false);
+        initialization();
+        listeners();
+        return view;
+    }
+
+    private void listeners() {
+        loginwitheamilbtn.setOnClickListener(this);
+    }
+
+    private void initialization() {
+
+        loginwithfbbtn = (Button) view.findViewById(R.id.loginwithfb);
+        loginwitheamilbtn = (Button) view.findViewById(R.id.loginwithemail);
+        registrationtv = (TextView) view.findViewById(R.id.register);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.loginwithemail:
+
+                Toast.makeText(getContext(), "hello", Toast.LENGTH_SHORT).show();
+              //  Log.d("abcd","abcd");
+                break;
+
+            default:
+                break;
+        }
     }
 }
