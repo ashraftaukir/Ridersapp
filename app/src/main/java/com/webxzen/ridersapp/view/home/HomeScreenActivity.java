a8d0eeb66201cdd5be7a2e56a4685641274d27eb
ashraftaukir <ashraftaukir@gmail.com>
2017-09-16 14:14:16 +0600

    Home screen map integration started.....

--- app/src/main/java/com/webxzen/ridersapp/view/home/HomeScreenActivity.java
+++ app/src/main/java/com/webxzen/ridersapp/view/home/HomeScreenActivity.java
@@ -5,17 +5,36 @@ import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.webxzen.ridersapp.R;
 
 
-public class HomeScreenActivity extends AppCompatActivity{
+public class HomeScreenActivity extends AppCompatActivity implements OnMapReadyCallback{
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.homescreenactivity);
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
 
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions().position(sydney)
+        //        .title("Marker in Sydney")
+
+
+        );
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
